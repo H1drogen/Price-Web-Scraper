10118fe6a9b25ef10cb41e35873612b3317fa918
SimonH <professional.simonhossain@gmail.com>
2024-04-15 18:14:19 +0100

    Amazon Scraper Updates

--- Scrapers/Amazon.py
+++ Scrapers/Amazon.py
@@ -1,6 +1,10 @@
 import requests
+import asyncio
 from bs4 import BeautifulSoup
+from playwright.async_api import async_playwright
+from playwright.sync_api import sync_playwright
 
+<<<<<<< Updated upstream
 def amazon_price_and_title():
     url = 'https://www.amazon.co.uk/Intel%C2%AE-CoreTM-i9-12900F-Desktop-Processor/dp/B09MDFH5HY/ref=sr_1_1_sspa?crid=15BOJBU34RP30&dib=eyJ2IjoiMSJ9.aG27SdaOhKl1C6IQGZWQVIBNrZVqDLlVVgNSaRcPdeVpPBg9zUyhnrYcprQqFl2Tsb_rqqN0W5rISuUYRaH3wskxImq3l-ziOfwzCMB0P79aMi2m5IjuXNPm-50o8CWuedSS63RcJP3KwjU9syH1uOzWsYbarI2uh4nn-NqnCBPEotNrXx7zkOacnLk1ck5iinEpX5PZHEvymcAunlBM8dsGPrBgJ-etlphMEZ6Fw2I.HJjDesTgG4xAiH2KhPhQB_v5HwPWwuCU7tLjhq7cpgs&dib_tag=se&keywords=intel+cpu&qid=1711734105&sprefix=intel+cpu%2Caps%2C70&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1'
     custom_headers = {
@@ -13,5 +17,48 @@ def amazon_price_and_title():
 
     title = soup.select_one('#productTitle').text.strip()
     price = soup.select_one('span.a-offscreen').text
+=======
+async def AmazonURL(product_name):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
 
-print(title, price)
+        # Navigate to Amazon's homepage
+        await page.goto('https://www.amazon.com')
+
+        # Find the search bar, type in the product name, and submit the search
+        await page.fill('#twotabsearchtextbox', product_name)
+        await page.press('#twotabsearchtextbox', 'Enter')
+>>>>>>> Stashed changes
+
+        # Wait for the search results page to load
+        await page.wait_for_load_state('networkidle')
+
+        # Collect the URL of the first product in the search results
+        first_product = await page.query_selector('.s-result-item .a-link-normal')
+        first_product_url = await first_product.get_attribute('href')
+
+        await browser.close()
+
+        return first_product_url
+
+
+
+
+def AmazonScraper(url):
+    url = 'https://www.amazon.co.uk/Intel%C2%AE-CoreTM-i9-12900F-Desktop-Processor/dp/B09MDFH5HY/ref=sr_1_1_sspa?crid=15BOJBU34RP30&dib=eyJ2IjoiMSJ9.aG27SdaOhKl1C6IQGZWQVIBNrZVqDLlVVgNSaRcPdeVpPBg9zUyhnrYcprQqFl2Tsb_rqqN0W5rISuUYRaH3wskxImq3l-ziOfwzCMB0P79aMi2m5IjuXNPm-50o8CWuedSS63RcJP3KwjU9syH1uOzWsYbarI2uh4nn-NqnCBPEotNrXx7zkOacnLk1ck5iinEpX5PZHEvymcAunlBM8dsGPrBgJ-etlphMEZ6Fw2I.HJjDesTgG4xAiH2KhPhQB_v5HwPWwuCU7tLjhq7cpgs&dib_tag=se&keywords=intel+cpu&qid=1711734105&sprefix=intel+cpu%2Caps%2C70&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1'
+    custom_headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0',
+        'accept-language': 'en-GB,en;q=0.5',
+    }
+
+    response = requests.get(url, headers=custom_headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    title = soup.select_one('#productTitle').text.strip()
+    price = soup.select_one('span.a-offscreen').text
+
+    print(title, price)
+
+
+product_url = asyncio.run(AmazonURL('Intel Core i9 12900F'))
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from Scrapers.Amazon import AmazonScraper
 app = Flask(__name__)
 
 
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/Amazon')
+def amazon_scraper():
+    product_url = asyncio.run(run('product name'))
+    product_info = AmazonScraper(product_url)
 
 if __name__ == '__main__':
     app.run()
