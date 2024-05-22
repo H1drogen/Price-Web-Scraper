b4853d338de44b9f2d069fbdba15fce7d1a65567
SimonH <professional.simonhossain@gmail.com>
2024-05-22 23:54:14 +0100

    Update API endpoint

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
-@app.route('/Tech/<product_url>')
+@app.route('/tech/<product_url>')
 def amazon_scraper(product_url):
     # product_url = asyncio.run(run('product name'))
     amazon_title, amazon_price = AmazonScraper(product_url)
